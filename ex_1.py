44719a005301348995a02fc87786935a2408af22
salokin-rm <82483428+salokin-rm@users.noreply.github.com>
2023-06-26 20:45:28 -0300

    Add files via upload

---
+++ ex_1.py
@@ -0,0 +1,18 @@
+"""
+Programa: Hello, World!
+Descrição: Este programa imprime na tela a frase "Hello, World!".
+Autor: Nikolas Martins
+Data: 21/06/2023
+Versão: 0.0.1
+
+"""
+
+# Atribuição de variáveis
+
+# Entrada de dados
+
+# Processamento de dados
+
+# Saída de dados
+
+print("Hello, World!")
